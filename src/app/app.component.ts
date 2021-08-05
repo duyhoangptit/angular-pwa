ab5555e9142b13121289d57555462396499a907b
hoangtd <hoangtd@gmail.com>
2021-08-05 07:04:50 +0700

    add pwa to project angular v12

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { RestApiService } from './rest-api.service';
+import {MatTableDataSource} from "@angular/material/table";
+import {MatPaginator} from "@angular/material/paginator";
+import {SwPush} from "@angular/service-worker";
+
+export interface TableElement {
+  id: string;
+  name: string;
+  email: string;
+  website: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'angular-pwa';
+  Data: TableElement[];
+  col: string[] = ['id', 'name', 'email', 'website'];
+  dataSource = new MatTableDataSource<TableElement>(this.Data);
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+
+  constructor(private restApiService: RestApiService, private push: SwPush) {
+    this.push.notificationClicks.subscribe(res => {
+      alert(res);
+    });
+    this.restApiService.getUsers().subscribe((res) => {
+      this.dataSource = new MatTableDataSource<TableElement>(res);
+      setTimeout(() => {
+        this.dataSource.paginator = this.paginator;
+      }, 0);
+    })
+  }
+
 }
